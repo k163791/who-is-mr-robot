cec19ad74afb27f8264c62c7a7fb092751507471
k163791 <k163791@nu.edu.pk>
2019-07-05 09:57:13 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox'
+// import Scroll from './Scroll';
+import './App.css'
+import robots from './robots'
+//PROPS are properties that we pass down to use from
+//one component to another
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+//STATE simply means the description of your app
+//simply an object that describes your application
+
+//Children is a way to render a way for something
+
+class App extends Component{
+
+	constructor()
+	{
+		super();
+		this.state = {
+			robots : robots,
+			searchField: '',
+		}
+	}
+
+	// componentDidMount()
+//	{
+//		fetch('https://jsonplaceholder.typicode.com/users').then(response =>
+//			{
+//				return response.json();
+//			})
+//		.then(users => this.setState({robots : users}));
+//	}
+
+	onSearchChange = (event) => {
+		this.setState({searchField: event.target.value});
+	}
+
+	render() {
+	const filteredRobots = this.state.robots.filter(robots =>
+	{
+		return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+	});
+
+	return(
+		<div className='tc'>
+			<h1 className='f1'>WHOISMR.ROBOT ? </h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			
+				<CardList robots={filteredRobots}/>
+		</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+	{
+		id : 1,
+		name : 'Uzair Hussain',
+		email : 'uzairhussain@gmail.com',
+	},
+
+	{
+		id : 2,
+		name : 'Alishan',
+		email : 'alishan@gmail.com',
+	},
+
+	{
+		id : 3,
+		name : 'Muzammil Asrar',
+		email : 'muzammil@gmail.com',
+	},
+
+	{
+		id : 4,
+		name : 'Ahsun Asim',
+		email : 'ahsun@gmail.com',
+	},
+
+	{
+		id : 5,
+		name : 'Taha Ahmed',
+		email : 'taha@gmail.com',
+	},
+
+	{
+		id : 6,
+		name : 'Saif Akhter',
+		email : 'saifu@gmail.com',
+	},
+
+	{
+		id : 7,
+		name: 'Bilal Khan',
+		email: 'bilal@gmail.com'
+	},
+
+	{
+		id : 8,
+		name : 'Ramiz Zulfiqar',
+		email : 'ramez@gmail.com',
+	},
+
+	{
+		id : 9,
+		name : 'Behraj Khan',
+		email : 'behraj@nu.edu.pk'
+	},
+
+	{
+		id : 10,
+		name : 'Sanghaar Magsi',
+		email : 'sanghaar.farigh@gmail.com'
+	},
+
+	{
+		id : 11,
+		name : 'Elliot Alderson',
+		email : 'samsepiol@allsafe.com',
+	},
+
+	{
+		id : 12,
+		name : 'Tyrell Wellick',
+		email : 'tyrell.wellick@ecorp.com'
+	}
+];
+
+export default robots;
